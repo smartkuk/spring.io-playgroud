f8c8ae85d9c9d4cc652e9a7b3815b16f56c818cb
SmartKuk <pipe0502@Smarts-MacBook-Pro.local>
2019-06-22 22:15:11 +0900

    apply gitignore

--- jpa-auditing-play/src/test/java/smart/kuk/person/AuditingTests.java
+++ jpa-auditing-play/src/test/java/smart/kuk/person/AuditingTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,10 +17,18 @@ public class AuditingTests {
 
   @Test
   public void test_jpa_auditing() {
+
+    LocalDateTime past = LocalDateTime.now();
     Person person = new Person("kuk", 10);
     personRepository.save(person);
 
     List<Person> persons = personRepository.findAll();
+
     assertThat(persons).isNotNull();
+    assertThat(persons.isEmpty()).isFalse();
+
+    Person firstMan = persons.get(0);
+
+    assertThat(past.isBefore(firstMan.getCreatedDate())).isTrue();
   }
 }
